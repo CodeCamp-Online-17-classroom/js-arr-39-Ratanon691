285907a36f5261424f91e5dcdc9ca2816f9d9143
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-07-08 10:23:49 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// write code here
+const array1 = ['Elephant', 'Ant', 'Cat', 'Eagle', 'Zebra'];
+// ค่าที่ขึ้นต้นด้วยตัว E
+const result1 = array1.filter(str => str.startsWith(`E`))
+
+const array2 = ['APPLE', 'oRanGE', 'PEACH', 'PaPAYA'];
+// ค่าที่เป็นตัวพิมพ์ใหญ่ทั้งหมด
+const result2 = array2.filter(str => str === str.toUpperCase())
+
+const array3 = ['Krabi', 'Chonburi', 'Buriram', 'Saraburi','Phrae'];
+// ค่าที่มีคำว่า buri เป็นส่วนประกอบโดยไม่สนใจตัวพิมพ์เล็กหรือตัวพิมพ์ใหญ่
+const result3 = array3.filter(str => str.toLowerCase().includes(`buri`))
